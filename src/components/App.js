52706f42dec4504bc0c42a9e8014f3ad4f39dfcc
rebeccabayham <becca.bayham@gmail.com>
2022-02-06 21:10:20 -0500

    Created react app

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import '../style/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to my portfolio!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
